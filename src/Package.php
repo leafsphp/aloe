fee6288b5f1a6a3145cbd1b5f4ac46e2924bd796
Mychi <mickdd22@gmail.com>
2021-10-20 11:44:14 +0000

    :sparkles: added leaf package

---
+++ src/Package.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Aloe;
+
+/**
+ * Package
+ * ----
+ * Meta info on aloe cli
+ */
+class Package
+{
+	/**
+	 * Check current version
+	 */
+	public static function info()
+	{
+		return json_decode(file_get_contents(
+			dirname(__DIR__) . "/composer.json"
+		));
+	}
+
+	/**
+	 * Check current version
+	 */
+	public static function version()
+	{
+		$meta = static::info();
+
+		return $meta->version;
+	}
+
+	/**
+	 * Find latest stable version
+	 */
+	public static function ltsInfo()
+	{
+		$package = json_decode(
+			file_get_contents("https://repo.packagist.org/p2/leafs/aloe.json")
+		);
+
+		return $package->packages->{"leafs/aloe"}[0];
+	}
+
+	/**
+	 * Find latest stable version
+	 */
+	public static function ltsVersion()
+	{
+		$package = static::ltsInfo();
+
+		return $package->version;
+	}
+
+	/**
+	 * Check if there is an update available
+	 */
+	public static function findUpdates()
+	{
+		$currentVersion = static::version();
+		$latestVersion = static::ltsVersion();
+
+		if ($currentVersion !== $latestVersion) {
+			echo "Aloe CLI update available ($latestVersion). Run composer require leafs/aloe to update.\n\n";
+		}
+	}
+}
