1b525584cca31b3f56de07c8ae44c1f099b0cb68
Michael Darko <mickdd22@gmail.com>
2022-02-13 16:24:32 +0000

    :sparkles: added support for modifying migrations

--- src/Command/GenerateMigrationCommand.php
+++ src/Command/GenerateMigrationCommand.php
@@ -7,20 +7,31 @@ use Illuminate\Support\Str;
 
 class GenerateMigrationCommand extends Command
 {
-    protected static $defaultName = "g:migration";
-    public $description = "Create a new migration file";
-    public $help = "Create a new migration file";
+    protected static $defaultName = 'g:migration';
+    public $description = 'Create a new migration file';
+    public $help = 'Create a new migration file';
 
     protected function config()
     {
-        $this->setArgument("migration", "required", "migration file name");
+        $this->setArgument('migration', 'required', 'migration file name');
+        $this->setOption('table', 't', 'optional', 'The database table to work with');
     }
 
     protected function handle()
     {
         $userInput = strtolower(Str::snake(Str::plural($this->argument("migration"))));
+        $fileContent = \file_get_contents(__DIR__ . '/stubs/migration.stub');
+        $table = $this->option('table') ?? $userInput;
 
-        if (strpos($userInput, "create") === false) {
+        if (
+            strpos($userInput, 'add_') === 0 ||
+            strpos($userInput, 'change_') === 0 ||
+            strpos($userInput, 'update_') === 0 ||
+            strpos($userInput, 'edit_') === 0
+        ) {
+            $fileContent = \file_get_contents(__DIR__ . '/stubs/updateMigration.stub');
+            $userInput = Str::singular($userInput);
+        } else if (strpos($userInput, 'create') === false) {
             $userInput = Str::snake("create_$userInput");
         }
 
@@ -30,9 +41,11 @@ class GenerateMigrationCommand extends Command
         touch($file);
 
         $className = Str::studly($userInput);
-
-        $fileContent = \file_get_contents(__DIR__ . '/stubs/migration.stub');
-        $fileContent = str_replace(["ClassName", "tableName"], [$className, str_replace("create_", "", $userInput)], $fileContent);
+        $fileContent = str_replace(
+            ["ClassName", "tableName"],
+            [$className, str_replace("create_", "", $table)],
+            $fileContent
+        );
 
         file_put_contents($file, $fileContent);
 
