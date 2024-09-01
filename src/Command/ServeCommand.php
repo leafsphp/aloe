f51a15702ada95b70391930137deafa074315da1
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-09-01 15:48:18 +0300

    ServeCommand Revamp
    - redefined options `host`, `port`, `path`
    - check port openings

--- src/Command/ServeCommand.php
+++ src/Command/ServeCommand.php
@@ -13,18 +13,46 @@ class ServeCommand extends Command
     protected function config()
     {
         $this->setOption('port', 'p', 'optional', 'Port to run Leaf app on', _env('SERVER_PORT', 5500));
-        $this->setArgument('path', 'optional', 'Path to your app (in case you changed it)');
+        $this->setOption('path', 't', 'optional', 'Path to your app', getcwd().'/public');
+		$this->setOption('host', 's', 'optional', 'Your application host', 'localhost');
     }
 
     protected function handle()
     {
         $port = $this->option('port');
-        $path = $this->argument('path');
+        $path = $this->option('path');
+        $host = $this->option('host');
 
-        $this->writeln('Server started on ' . asComment("http://localhost:$port"));
-        $this->info("Happy gardening!!\n");
-        $this->writeln(shell_exec("php -S localhost:$port $path"));
+        # check if directory exists
+        if (!is_dir($path)) {
+            $this->error("Directory $path does not exist");
+            return;
+        }
 
-        return 0;
+        # check if port is in use by $host and localhost
+        $this->writeln('');
+        while (true) {
+            $defSocket = @fsockopen($host, $port, $errno, $errstr, 1);
+            $localSocket = @fsockopen('localhost', $port, $errno, $errstr, 1);
+
+            if($defSocket){
+                $this->error("Port $port is already in use by $host, trying port " . ($port + 1));
+                $port++;
+            } else{
+                
+                if(!$localSocket) break;
+
+                $this->error("WARNING:");
+                $this->writeln(asComment("While port $port is available on $host, it is already in use by localhost"));
+                break;
+            } 
+        }
+
+        $this->writeln(PHP_EOL. "Server started on " . asComment("http://$host:$port"));
+        $this->info("Happy gardening!!" . PHP_EOL);
+
+        $this->writeln(shell_exec("php -S $host:$port -t $path"));
+
+        return;
     }
 }
