0fa94e5651d984f2f9a8d92eff1990abedd704ab
mychidarko <mickdd22@gmail.com>
2024-11-26 09:32:15 +0000

    fix: patch up db install command

--- src/Command/DatabaseInstallCommand.php
+++ src/Command/DatabaseInstallCommand.php
@@ -12,16 +12,22 @@ class DatabaseInstallCommand extends Command
 
     protected function handle()
     {
-        $host = getenv('DB_HOST');
-        $user = getenv('DB_USERNAME');
-        $password = getenv('DB_PASSWORD');
-        $database = getenv('DB_DATABASE');
-        $port = empty(getenv('DB_PORT')) ? 3306 : getenv('DB_PORT');
+        $host = _env('DB_HOST');
+        $user = _env('DB_USERNAME');
+        $password = _env('DB_PASSWORD');
+        $database = _env('DB_DATABASE');
+        $dbConnection = _env('DB_CONNECTION', 'mysql');
+        $port = empty(_env('DB_PORT')) ? 3306 : _env('DB_PORT');
 
-        if (\mysqli_query(
-            \mysqli_connect($host, $user, $password, '', (int) $port),
-            "CREATE DATABASE `$database`"
-        )) {
+        db()->connect([
+            'dbtype' => MvcConfig('database')['connections'][$dbConnection]['driver'] ?? 'mysql',
+            'host' => $host,
+            'username' => $user,
+            'password' => $password,
+            'port' => $port,
+        ]);
+
+        if (db()->create($database)->execute()) {
             $this->info("$database created successfully.");
             return 0;
         }
