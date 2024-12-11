8bffbfe87c2fac67db348a2f2f5f03dcf6b03f3d
mychidarko <mickdd22@gmail.com>
2024-12-11 22:48:13 +0000

    feat: update migrate command to use schema

--- src/Command/DatabaseMigrationCommand.php
+++ src/Command/DatabaseMigrationCommand.php
@@ -8,89 +8,46 @@ use Illuminate\Support\Str;
 class DatabaseMigrationCommand extends Command
 {
     protected static $defaultName = 'db:migrate';
-    public $description = 'Run the database migrations';
+    public $description = 'Migrate your db schema files';
     public $help = "Run the migrations defined in the migrations directory\n";
 
     protected function config()
     {
-        $this->setOption('file', 'f', 'optional', 'Rollback a particular file');
+        $this->setArgument('file', 'optional', 'Rollback a particular file');
         $this->setOption('seed', 's', 'none', 'Run seeds after migration');
     }
 
     protected function handle()
     {
-        $fileToRollback = $this->option('file');
-        $migrations = glob(Config::rootpath(MigrationsPath('*.php')));
+        $fileToMigrate = $this->argument('file');
+        $migrations = glob(Config::rootpath(AppPaths('database') . DIRECTORY_SEPARATOR . '*.yml'));
 
         foreach ($migrations as $migration) {
-            $file = pathinfo($migration);
-            $filename = $file['filename'];
+            $currentFileName = path($migration)->basename();
 
-            if ($filename !== 'Schema') :
-                $className = Str::studly(\substr($filename, 17));
+            if ($fileToMigrate && rtrim($currentFileName, '.yml') !== rtrim($fileToMigrate, '.yml')) {
+                continue;
+            }
 
-                if ($fileToRollback) {
-                    if (strpos($migration, Str::snake("_create_$fileToRollback.php")) !== false) {
-                        $this->migrate($className, $filename);
-                        $this->info('db migration on ' . asComment(str_replace(Config::rootpath(MigrationsPath()), '', $migration)));
+            $this->writeln("> db migration on <comment>$currentFileName</comment>");
 
-                        if ($this->option('seed')) {
-                            $seederClass = $this->seedTable(str_replace(
-                                ['Create'],
-                                '',
-                                Str::studly(\substr($filename, 17))
-                            ));
+            if (!\Leaf\Schema::migrate($migration)) {
+                $this->error("Could not migrate $currentFileName");
+                return 1;
+            }
 
-                            if ($seederClass) {
-                                $this->writeln(asComment($seederClass) . ' seeded successfully!');
-                            }
-                        }
-
-                        $this->info("Database migration completed!\n");
-                        return 0;
-                    }
-
-                    continue;
-                } else {
-                    $this->migrate($className, $filename);
-                    $this->writeln('> db migration on ' . asComment(str_replace(Config::rootpath(MigrationsPath()), '', $migration)));
-
-                    if ($this->option('seed')) {
-                        $seederClass = $this->seedTable(str_replace(
-                            'Create',
-                            '',
-                            Str::studly(\substr($filename, 17))
-                        ));
-
-                        if ($seederClass) {
-                            $this->writeln(asComment($seederClass) . ' seeded successfully!');
-                        }
-                    }
+            if ($this->option('seed')) {
+                if (!\Leaf\Schema::seed($migration)) {
+                    $this->error("Could not seed $currentFileName");
+                    return 1;
                 }
-            endif;
+
+                $this->writeln("> $currentFileName seeded successfully!");
+            }
         }
 
         $this->info("Database migration completed!\n");
+
         return 0;
-    }
-
-    protected function migrate($className, $filename)
-    {
-        require_once Config::rootpath(MigrationsPath("$filename.php", false));
-
-        $class = new $className;
-        $class->up();
-    }
-
-    protected function seedTable($table)
-    {
-        $className = "\App\Database\Seeds\\" . Str::plural($table) . "Seeder";
-
-        if (!class_exists($className)) return false;
-
-        $class = new $className;
-        $class->run();
-
-        return $className;
     }
 }
