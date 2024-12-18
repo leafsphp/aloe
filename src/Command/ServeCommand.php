a33812fa2fa46aa38ffb379ec249e824766fb97e
mychidarko <mickdd22@gmail.com>
2024-12-18 22:57:55 +0000

    feat: add concurrent mode to serve command

--- src/Command/ServeCommand.php
+++ src/Command/ServeCommand.php
@@ -13,46 +13,62 @@ class ServeCommand extends Command
     protected function config()
     {
         $this->setOption('port', 'p', 'optional', 'Port to run Leaf app on', _env('SERVER_PORT', 5500));
-        $this->setOption('path', 't', 'optional', 'Path to your app', getcwd().'/public');
-		$this->setOption('host', 's', 'optional', 'Your application host', 'localhost');
+        $this->setOption('path', 't', 'optional', 'Path to your app', getcwd() . '/public');
+        $this->setOption('host', 's', 'optional', 'Your application host', 'localhost');
+        $this->setOption('no-concurrent', 'nc', 'optional', 'Run PHP server without Vite server', false);
     }
 
     protected function handle()
     {
+        $useConcurrent = true;
+
         $port = $this->option('port');
         $path = $this->option('path');
         $host = $this->option('host');
+        $noConcurrent = $this->option('no-concurrent');
 
-        # check if directory exists
-        if (!is_dir($path)) {
-            $this->error("Directory $path does not exist");
-            return;
+        if ($noConcurrent || !class_exists('Leaf\Vite') || !file_exists(getcwd() . '/vite.config.js')) {
+            $useConcurrent = false;
         }
 
-        # check if port is in use by $host and localhost
-        $this->writeln('');
+        if (!is_dir($path)) {
+            $this->error("Directory $path does not exist");
+            return 1;
+        }
+
         while (true) {
             $defSocket = @fsockopen($host, $port, $errno, $errstr, 1);
             $localSocket = @fsockopen('localhost', $port, $errno, $errstr, 1);
 
-            if($defSocket){
-                $this->error("Port $port is already in use by $host, trying port " . ($port + 1));
+            if ($defSocket) {
+                $this->write("Port $port is already in use by $host, trying port " . ($port + 1)) . "...";
                 $port++;
-            } else{
-                
-                if(!$localSocket) break;
-
-                $this->error("WARNING:");
-                $this->writeln(asComment("While port $port is available on $host, it is already in use by localhost"));
+            } else if (!$localSocket) {
                 break;
-            } 
+            } else {
+                $this->error('WARNING:');
+                $this->writeln(asComment("While port $port is available on $host, it is already in use by localhost"));
+
+                break;
+            }
         }
 
-        $this->writeln(PHP_EOL. "Server started on " . asComment("http://$host:$port"));
-        $this->info("Happy gardening!!" . PHP_EOL);
+        if ($useConcurrent) {
+            $this->writeln("\nVite detected, running Leaf server and Vite server concurrently\n");
+            $this->info("Happy gardening!!\n");
 
-        $this->writeln(shell_exec("php -S $host:$port -t $path"));
+            if (!file_exists(getcwd() . '/node_modules')) {
+                $this->writeln(shell_exec('npm i'));
+            }
 
-        return;
+            \Aloe\Core::run(
+                "npx concurrently -c \"#3eaf7c,#bd34fe\" \"php -S $host:$port -t $path\" \"npm run dev\" --names=server,vite --colors",
+                $this->output()
+            );
+        } else {
+            $this->writeln(shell_exec("php -S $host:$port -t $path"));
+        }
+
+        return 0;
     }
 }
