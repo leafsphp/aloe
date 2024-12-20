ddcb87881050de9082bd3123e29b11005dfeb993
mychidarko <mickdd22@gmail.com>
2024-12-20 16:02:10 +0000

    feat: add db:install to db:migrate

--- src/Command/DatabaseMigrationCommand.php
+++ src/Command/DatabaseMigrationCommand.php
@@ -50,4 +50,29 @@ class DatabaseMigrationCommand extends Command
 
         return 0;
     }
+
+    public function createDatabase()
+    {
+        $host = _env('DB_HOST');
+        $user = _env('DB_USERNAME');
+        $password = _env('DB_PASSWORD');
+        $database = _env('DB_DATABASE');
+        $dbConnection = _env('DB_CONNECTION', 'mysql');
+        $port = empty(_env('DB_PORT')) ? 3306 : _env('DB_PORT');
+
+        db()->connect([
+            'dbtype' => MvcConfig('database')['connections'][$dbConnection]['driver'] ?? 'mysql',
+            'host' => $host,
+            'username' => $user,
+            'password' => $password,
+            'port' => $port,
+        ]);
+
+        if (db()->create($database)->execute()) {
+            $this->info("$database created successfully.");
+            return 0;
+        }
+
+        $this->error("$database could not be created.");
+    }
 }
