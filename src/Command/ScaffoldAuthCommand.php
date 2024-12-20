c5eb129b7a65ef19956eb6cd223fe157105932b2
mychidarko <mickdd22@gmail.com>
2024-12-20 16:07:49 +0000

    feat: rename auth:scaffold to scaffold:auth

---
+++ src/Command/ScaffoldAuthCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Aloe\Command;
+
+use Aloe\Installer;
+use Illuminate\Support\Str;
+
+class ScaffoldAuthCommand extends \Aloe\Command
+{
+    protected static $defaultName = 'scaffold:auth';
+    public $description = 'Scaffold basic app authentication';
+    public $help = 'Create basic views, routes and controllers for authentication';
+
+    protected function config()
+    {
+        $this
+            ->setOption('session', 's', 'NONE', 'Use session/session + JWT instead of just JWT')
+            ->setOption('api', 'a', 'NONE', 'Use JWT for authentication');
+    }
+
+    protected function handle()
+    {
+        $driver = 'session';
+
+        if ($this->option('api')) {
+            $driver = 'api';
+        }
+
+        if (Config::$env === 'API' && !$this->option('session')) {
+            $driver = 'api';
+        }
+
+        $installablesDir = $this->installable($driver);
+
+        Installer::magicCopy($installablesDir);
+
+        $this->info('Authentication generated successfully.');
+
+        return 0;
+    }
+
+    protected function installable($driver)
+    {
+        return dirname(__DIR__) . '/Scaffold/' .  Str::studly("{$driver}Auth");
+    }
+}
