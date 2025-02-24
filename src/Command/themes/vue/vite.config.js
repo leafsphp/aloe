0009e0ed9894f73044ab002fc853f46ce32c9954
mychidarko <mickdd22@gmail.com>
2025-02-24 12:31:10 +0000

    fix: add missing tailwind setup code to inertia

--- src/Command/themes/vue/vite.config.js
+++ src/Command/themes/vue/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import leaf from '@leafphp/vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         leaf({
-            input: ['app/views/js/app.jsx'],
+            input: ['app/views/js/app.js'],
             refresh: true,
         }),
         vue({
