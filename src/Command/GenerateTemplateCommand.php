6f7ad2bc9476f5f4309fe1fe52f6efe30cab12d4
mychidarko <mickdd22@gmail.com>
2025-02-24 14:45:49 +0000

    feat: automatically detect view engine without --type

--- src/Command/GenerateTemplateCommand.php
+++ src/Command/GenerateTemplateCommand.php
@@ -8,20 +8,40 @@ class GenerateTemplateCommand extends \Aloe\Command
     public $description = 'Create a new view file';
     public $help = 'Create a new basic view file';
 
+    protected $type = 'blade';
+
     protected function config()
     {
         $this
             ->setAliases(['g:view'])
             ->setArgument('name', 'REQUIRED', 'The name of the template to create')
-            ->setOption('type', 't', 'OPTIONAL', 'The type of template to create: jsx, vue, svelte, blade', 'blade');
+            ->setOption('type', 't', 'OPTIONAL', 'The type of template to create: jsx, vue, svelte, blade');
     }
 
     protected function handle()
     {
+        $directory = Config::rootpath();
+
+        if (\Leaf\FS\File::exists("$directory/app/views/_inertia.blade.php")) {
+            $content = \Leaf\FS\File::read("$directory/app/views/_inertia.blade.php");
+
+            if (strpos($content, '.jsx') !== false) {
+                $this->type = 'react';
+            } else if (strpos($content, '.svelte') !== false) {
+                $this->type = 'svelte';
+            } else if (strpos($content, '.vue') !== false) {
+                $this->type = 'vue';
+            }
+        }
+
+        if ($this->option('type')) {
+            $this->type = $this->option('type');
+        }
+
         $templateName = strtolower($this->argument('name'));
         $templateName = $this->getTemplateName($templateName);
         $template = Config::rootpath(ViewsPath(
-            $this->option('type') === 'blade' ? $templateName : "/js/$templateName"
+            $this->type === 'blade' ? $templateName : "/js/$templateName"
         ));
 
         storage()->createFile($template, function () {
@@ -39,14 +59,19 @@ class GenerateTemplateCommand extends \Aloe\Command
 
     protected function getTemplateName($templateName)
     {
-        return $this->option('type') === 'blade'
+        if ($this->type !== 'blade') {
+            $templateName = implode('/', array_map('ucfirst', explode('/', $templateName)));
+        }
+
+        return $this->type === 'blade'
             ? "$templateName.blade.php"
-            : (\Illuminate\Support\Str::studly($templateName) . '.' . $this->option('type'));
+            : ($this->type === 'react'
+                ? "$templateName.jsx" : ("$templateName.{$this->type}"));
     }
 
     protected function generateTemplateData()
     {
-        $type = $this->option('type');
+        $type = $this->type;
         $stub = \file_get_contents(__DIR__ . "/stubs/template/$type.stub");
 
         return $stub;
