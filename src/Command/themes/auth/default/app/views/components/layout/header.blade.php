32e05ab6143823ca557719c6e7e027383d21518d
mychidarko <mickdd22@gmail.com>
2025-02-28 18:28:35 +0000

    chore: update version

--- src/Command/themes/auth/default/app/views/components/layout/header.blade.php
+++ src/Command/themes/auth/default/app/views/components/layout/header.blade.php
@@ -71,7 +71,7 @@
                     <div data-sidebar="group-content" class="w-full text-sm">
                         <ul data-sidebar="menu" class="flex w-full min-w-0 flex-col gap-1">
                             <li data-sidebar="menu-item" class="group/menu-item relative"><a
-                                    href="https://github.com/laravel/vue-starter-kit" target="_blank"
+                                    href="https://github.com/leafsphp/leaf" target="_blank"
                                     rel="noopener noreferrer" data-sidebar="menu-button" data-size="default"
                                     data-active="false"
                                     class="peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left outline-none ring-sidebar-ring transition-[width,height,padding] focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 hover:bg-sidebar-accent h-8 text-sm text-neutral-600 hover:text-neutral-800 dark:text-neutral-300 dark:hover:text-neutral-100"><svg
@@ -84,7 +84,7 @@
                                         </path>
                                     </svg><span>Github Repo</span></a></li>
                             <li data-sidebar="menu-item" class="group/menu-item relative"><a
-                                    href="https://laravel.com/docs/starter-kits" target="_blank"
+                                    href="https://leafphp.dev" target="_blank"
                                     rel="noopener noreferrer" data-sidebar="menu-button" data-size="default"
                                     data-active="false"
                                     class="peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left outline-none ring-sidebar-ring transition-[width,height,padding] focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 hover:bg-sidebar-accent h-8 text-sm text-neutral-600 hover:text-neutral-800 dark:text-neutral-300 dark:hover:text-neutral-100"><svg
