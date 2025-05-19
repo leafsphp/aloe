db4793f78cdfe1187a073d3b5dc1490b90c5d789
mychidarko <mickdd22@gmail.com>
2025-05-19 10:20:03 +0000

    fix: update fs config

--- src/Command/stubs/config/fs.php
+++ src/Command/stubs/config/fs.php
@@ -35,6 +35,7 @@ return [
             'bucket' => _env('AWS_BUCKET'),
             'url' => _env('AWS_URL'),
             'endpoint' => _env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => _env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
     ],
 
